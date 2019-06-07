7de6a99b01dc8f7094c614c45d98054d7fe29278
simon <epic.fail6174@googlemail.com>
2019-06-07 21:59:45 +0200

    Minus pollution

--- stack_on_black.py
+++ stack_on_black.py
@@ -33,15 +33,59 @@ def stack_images_on_black(files, x_offset, y_offset, stride):
 	image_offsets = interpolate_offsets(len(files), x_offset, y_offset)
 
 	stacked_image = create_image(total_width, total_height, channels)
+	samples = np.zeros((total_width, total_height))
+
 	for index, (x,y) in enumerate(image_offsets):		
 		if index % stride != 0:
 			continue
 		image = load_image(files[index])
 		padded_image = create_image(total_width, total_height, channels)
 		padded_image[x:x+image_width, y:y+image_height, :] = image		
+		
 		stacked_image = np.add(stacked_image, padded_image)
 
-	return stacked_image
+		samples[x:x+image_width, y:y+image_height] += 1
+
+	return stacked_image, samples
+
+
+def substract_pollution(image, samples):
+	max_samples = np.amax(samples)
+	min_pollution = None
+
+	width, height, channels = image.shape
+	for x in range(width):
+		for y in range(height):
+			if samples[x,y] == max_samples:
+				for c in range(channels):
+					pollution = image[x,y,c]
+					if min_pollution is None or pollution < min_pollution:
+						min_pollution = pollution 
+
+	min_value = np.amin(image)
+	max_value = np.amax(image)	
+	print(f'min={min_value}, max={max_value}, max samples={max_samples}, pollution={min_pollution}')
+
+	pollution_image = samples.astype(float) / float(max_samples) * float(min_pollution) 
+	
+	result = np.zeros(image.shape, dtype=float)
+	for channel in range(channels):
+		result[:, :, channel] = image[:, :, channel] - pollution_image
+
+	result = np.clip(result, 0.0, 1.0)
+	return result
+
+
+def normalize_image(image):
+	min_value = np.amin(image)
+	max_value = np.amax(image)
+	if max_value == 0:
+		print('Not normalizing image: It is all black.')		
+		return image
+
+	float_image = image.astype(float)
+	normalized = (float_image - min_value) / (max_value - min_value)
+	return normalized
 
 
 if __name__ == '__main__':
@@ -64,6 +108,31 @@ if __name__ == '__main__':
 	x_offset = int(offsets[0])
 	y_offset = int(offsets[1])
 
-	stacked_image = stack_images_on_black(files, x_offset, y_offset, stride)
-	stacked_image = maximize_contrast(stacked_image, max_channel_value=255.0)
-	save_image(stacked_image, 'stacked_on_black.png', rgb_mode=True)
+	image, samples = stack_images_on_black(files, x_offset, y_offset, stride)
+
+	#cx, cy, r = 2583, 934, 400
+	#image = image[cx-r:cx+r, cy-r:cy+r, :]
+	#samples = samples[cx-r:cx+r, cy-r:cy+r]
+
+	print('stacked', len(np.unique(image)))
+	
+	image = normalize_image(image)
+	print('normalized', len(np.unique(image)))
+	
+	image = substract_pollution(image, samples)
+	print('depolluted', len(np.unique(image)))
+	
+	image = normalize_image(image)
+	print('normalized', len(np.unique(image)))
+	
+	#image = np.square(image)
+	#image = np.square(image)
+	#image = np.sqrt(image)
+	print('filtered', len(np.unique(image)))
+	
+	image = (255.0 * image).astype(np.int16)
+	print('output', len(np.unique(image)))
+
+	# stacked_image = maximize_contrast(stacked_image, max_channel_value=255.0)
+	
+	save_image(image, 'stacked_on_black.png', rgb_mode=True)

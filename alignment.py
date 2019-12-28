d4ce46f7430982ada6610f6c25f8ac3958381848
simon <epic.fail6174@googlemail.com>
2019-12-28 19:46:22 +0100

    Some changes

--- alignment.py
+++ alignment.py
@@ -11,23 +11,20 @@ from skimage.feature import register_translation
 
 class Alignment:
 
-	def __init__(self, amplification=5, threshold=128, max_frame_distance=None):
+	def __init__(self, amplification=5, threshold=128):
 		# factor by which brightness should be scaled up for alignment
 		self.amplification = amplification
 		# brightness threshold to apply before alignment
 		self.threshold = threshold
-		# maximum distance to allow between frames without introducing a new keyframe
-		self.max_frame_distance = max_frame_distance
 
 		self.key_frame = None
-		self.key_frame_index = 0
-		self.prev_frame = None
 
 		# map frame index to offsets-tuple
 		self.frame_offsets = {0: (0,0)}
 
 	def get_offsets(self, frame, frame_index):
 		# apply measures to improve offset detection on our kind of images
+		# TODO use those sneaky sigma-tricks from the astrometry guys
 		curr_frame = np.clip(frame * self.amplification, self.threshold, 255)
 
 		if frame_index == 0:
@@ -35,26 +32,6 @@ class Alignment:
 			return (0,0)
 
 		(offset_x, offset_y), error, _ = register_translation(self.key_frame, curr_frame)
-		cartesian_offset = math.sqrt(offset_x*offset_x + offset_y*offset_y)
-		
-		if cartesian_offset > self.max_frame_distance:
-			if self.key_frame_index == frame_index-1:
-				print('Too big offset between adjacent frames. Aborting.')
-				sys.exit(1)
-
-			print(f'Too big offset between frame and key frame. Using frame {frame_index-1} as key new frame.')
-			self.key_frame = self.prev_frame
-			self.key_frame_index = frame_index-1
-
-			(offset_x, offset_y), error, _ = register_translation(self.key_frame, curr_frame)
-			cartesian_offset = math.sqrt(offset_x*offset_x + offset_y*offset_y)
-
-		key_frame_offset = self.frame_offsets[self.key_frame_index]
-		offset_x += key_frame_offset[0]
-		offset_y += key_frame_offset[1]
-		
-		self.prev_frame = curr_frame
-
 		offsets = (offset_x, offset_y)
 		self.frame_offsets[frame_index] = offsets
 		return offsets
--- analyze.py
+++ analyze.py
@@ -3,6 +3,8 @@ import glob
 import argparse
 import os
 import json
+import math
+import itertools
 
 import numpy as np
 
@@ -15,7 +17,6 @@ from skimage.transform import rescale
 parser = argparse.ArgumentParser()
 parser.add_argument('directory', type=str)
 parser.add_argument('--filename-pattern', type=str, default='*.tif', help='Pattern to use when searching for input images')
-parser.add_argument('--max-frame-distance', type=int, default=480, help='Maximum cartesian distance between frames, in pixels')
 parser.add_argument('--range', type=str, default=None, help='Stack only given range of images, not all')
 parser.add_argument('--amplification', type=int, default=5, help='Multiply images by this number before offset detection')
 parser.add_argument('--threshold', type=int, default=128, help='Clip images by this brightness value after amplification')
@@ -38,31 +39,54 @@ if args.range is not None:
 	files = files[int(image_range[0]):int(image_range[1])]
 	print(f'Only {len(files)} files selected for offset detection')
 
-alignment = Alignment(args.amplification, args.threshold, args.max_frame_distance)
+alignment = Alignment(args.amplification, args.threshold)
 
 frame_offsets_by_file = dict()	# map filename to offsets tuple
+frame_offsets = []
+
 for frame_index, file in enumerate(files):
 	basename = os.path.basename(file)
 	frame = load_image_greyscale(file)
 	offsets = alignment.get_offsets(frame, frame_index)
+	
 	frame_offsets_by_file[basename] = offsets
+	frame_offsets.append(offsets)
 	
 	# frame = rescale(frame, 1.0/4.0, multichannel=False)
 	# frame = (32768 * frame).astype(np.int16)
 	# save_image_greyscale(frame, f'downsampled/{frame_index}.png')
 
-	sharpness_aog = get_sharpness_aog(frame)
-	sharpness_vol = get_sharpness_vol(frame)
-	sharpness_sobel = get_sharpness_sobel(frame)
-	brightness = np.average(frame)
+	# sharpness_aog = get_sharpness_aog(frame)
+	# sharpness_vol = get_sharpness_vol(frame)
+	# sharpness_sobel = get_sharpness_sobel(frame)
+	# brightness = np.average(frame)
 
 	print(f'({frame_index+1}/{len(files)}) '\
 		  f'Name={basename}, '\
-		  f'Offset={offsets}, '\
-		  f'Sharpness(AoG)={sharpness_aog:.2f}, '\
-		  f'Sharpness(VoL)={sharpness_vol:.10f}, '\
-		  f'Sharpness(VoL)={sharpness_sobel:.10f}, '\
-		  f'Brightness={brightness:.10f}')
+		  f'Offset={offsets}')\
+	#	  f'Sharpness(AoG)={sharpness_aog:.2f}, '\
+	#	  f'Sharpness(VoL)={sharpness_vol:.10f}, '\
+	#	  f'Sharpness(VoL)={sharpness_sobel:.10f}, '\
+	#	  f'Brightness={brightness:.10f}')
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+def distance(p1, p2):
+	return math.sqrt((p2[0]-p1[0])*(p2[0]-p1[0]) + (p2[1]-p1[1])*(p2[1]-p1[1]))
+
+distances = [distance(p1, p2) for p1, p2 in pairwise(frame_offsets)]
+instability = [max(d1, d2) for d1, d2 in pairwise(distances)]
+
+def runs(instability, thresh):
+	below_thresh = [inst < thresh for inst in instability]
+	# ...
+
+print(instability)
+
 
 if not args.dryrun:
 	offsets_file = os.path.join(args.directory, 'offsets.json')

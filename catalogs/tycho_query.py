e67b2294336e49830c95709d922c05182d58dcf3
simon <epic.fail6174@googlemail.com>
2020-10-11 14:36:32 +0200

    A simple plot of the catalog for my personal entertainment

--- catalogs/tycho_query.py
+++ catalogs/tycho_query.py
@@ -10,13 +10,26 @@ connection = sqlite3.connect(db_file)
 result = connection.execute('''
     SELECT *
     FROM stars
-    WHERE dec > 0 AND dec < 10 AND ra > 0 AND ra < 10
     ORDER BY mag
-    LIMIT 10
-    OFFSET 100
+    LIMIT 25000
 ''')
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+xs = []
+ys = []
 for row in result:
-    print(row)
+    xs.append(row[1])
+    ys.append(row[2])
+
+xs = np.array(xs)
+ys = np.array(ys)
+
+plt.scatter(xs, ys, s=1, c='#000000', alpha=0.3)
+plt.title('Stars')
+plt.xlabel('RA')
+plt.ylabel('Dec')
+plt.savefig('starplot.png', dpi=400)
 
 connection.close()

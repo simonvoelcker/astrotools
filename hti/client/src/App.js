97a8c2cc14a0e89c7df9fe55a6c6706049ba0ffa
simon <epic.fail6174@googlemail.com>
2021-09-21 21:30:38 +0200

    Frontend simplifications

--- hti/client/src/App.js
+++ hti/client/src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import { AppProvider } from './context/AppContext'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/css/main.sass'
 
 import Operator from './components/views/Operator'
-import Viewer from './components/views/Viewer'
 
 class App extends Component {
   render () {
     return (
       <AppProvider value={this.state}>
-        <Router>
-          <div className='App'>
-            <Switch>
-              <Route path='/' component={Operator} exact />
-              <Route path='/viewer' component={Viewer} exact />
-              <Route>
-                <Redirect to='/' />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
+        <Operator />
       </AppProvider>
     )
   }
